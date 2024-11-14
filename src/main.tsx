5ae24b70043c49022a0241088bc6583303ac6cef
mircocosov <Artem22832@mail.ru>
2024-11-14 09:54:08 +0300

    feat config

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Header from "./pages/Header/Header.tsx"
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world</div>,
+    element: <Header />,
   },
 ])
 
