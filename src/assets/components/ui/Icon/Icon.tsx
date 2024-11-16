c77ac07d5b1bfafa2d77c80a9daf45b196a6c76c
mircocosov <Artem22832@mail.ru>
2024-11-16 16:03:48 +0300

    feat: menu & globe svg

--- src/assets/components/ui/Icon/Icon.tsx
+++ src/assets/components/ui/Icon/Icon.tsx
@@ -1,42 +1,74 @@
 import styles from "./Icon.module.scss"
 import cn from "classnames"
 
+import Basket from "@/assets/icons/basket.svg"
 import BasketballIcon from "@/assets/icons/basketball.svg"
 import CameraIcon from "@/assets/icons/camera.svg"
+import Coinbase from "@/assets/icons/coinbase.svg"
+import Discord from "@/assets/icons/discord.svg"
+import Envelope from "@/assets/icons/envelope.svg"
+import Eye from "@/assets/icons/eye.svg"
+import Folder from "@/assets/icons/folder.svg"
+import Globe from "@/assets/icons/globe.svg"
+import Instagram from "@/assets/icons/instagram.svg"
 import LogoIcon from "@/assets/icons/logo.svg"
 import MagicStickIcon from "@/assets/icons/magic-stick.svg"
+import Menu from "@/assets/icons/menu.svg"
+import Metamask from "@/assets/icons/metamask.svg"
 import MusicNotesIcon from "@/assets/icons/music-notes.svg"
 import PaintBrushIcon from "@/assets/icons/paint-brush.svg"
 import PlanetIcon from "@/assets/icons/planet.svg"
+import Purse from "@/assets/icons/purse.svg"
 import RocketIcon from "@/assets/icons/rocket.svg"
 import SwatchesIcon from "@/assets/icons/swatches.svg"
+import Twitter from "@/assets/icons/twitter.svg"
 import UserIcon from "@/assets/icons/user.svg"
 import VideoCameraIcon from "@/assets/icons/video.svg"
-import Envelope from "@/assets/icons/envelope.svg"
-import Eye from "@/assets/icons/eye.svg"
-import Discord from "@/assets/icons/discord.svg"
-import Instagram from "@/assets/icons/instagram.svg"
-import Twitter from "@/assets/icons/twitter.svg"
-import Youtube from "@/assets/icons/youtube.svg"
-import Coinbase from "@/assets/icons/coinbase.svg"
-import Metamask from "@/assets/icons/metamask.svg"
 import Wallet from "@/assets/icons/wallet.svg"
-import Basket from "@/assets/icons/basket.svg"
-import Folder from "@/assets/icons/folder.svg"
-import Purse from "@/assets/icons/purse.svg"
+import Youtube from "@/assets/icons/youtube.svg"
 
 const ICONS = {
   basketball: {
     icon: BasketballIcon,
   },
+  basket: {
+    icon: Basket,
+  },
   camera: {
     icon: CameraIcon,
+  },
+  coinbase: {
+    icon: Coinbase,
+  },
+  discord: {
+    icon: Discord,
+  },
+  envelope: {
+    icon: Envelope,
+  },
+  eye: {
+    icon: Eye,
+  },
+  folder: {
+    icon: Folder,
+  },
+  globe: {
+    icon: Globe,
+  },
+  instagram: {
+    icon: Instagram,
   },
   logo: {
     icon: LogoIcon,
   },
   magicStick: {
     icon: MagicStickIcon,
+  },
+  menu: {
+    icon: Menu,
+  },
+  metamask: {
+    icon: Metamask,
   },
   musicNotes: {
     icon: MusicNotesIcon,
@@ -47,11 +79,17 @@ const ICONS = {
   planet: {
     icon: PlanetIcon,
   },
+  purse: {
+    icon: Purse,
+  },
   rocket: {
     icon: RocketIcon,
   },
   swatches: {
     icon: SwatchesIcon,
+  },
+  twitter: {
+    icon: Twitter,
   },
   user: {
     icon: UserIcon,
@@ -59,41 +97,11 @@ const ICONS = {
   videoCamera: {
     icon: VideoCameraIcon,
   },
-  envelope: {
-    icon: Envelope,
-  },
-  eye: {
-    icon: Eye,
-  },
-  discord: {
-    icon: Discord,
-  },
-  instagram: {
-    icon: Instagram,
-  },
-  twitter: {
-    icon: Twitter,
-  },
-  youtube: {
-    icon: Youtube,
-  },
-  coinbase: {
-    icon: Coinbase,
-  },
-  metamask: {
-    icon: Metamask,
-  },
   wallet: {
     icon: Wallet,
   },
-  basket: {
-    icon: Basket,
-  },
-  folder: {
-    icon: Folder,
-  },
-  purse: {
-    icon: Purse,
+  youtube: {
+    icon: Youtube,
   },
 }
 
