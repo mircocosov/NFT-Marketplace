2806076ab40acbcddae62985fd4f815d32282144
mircocosov <Artem22832@mail.ru>
2024-11-25 08:21:40 +0300

    fix: mixin bag

--- src/pages/Header/Header.tsx
+++ src/pages/Header/Header.tsx
@@ -1,8 +1,26 @@
 import style from "./Header.module.scss"
+import Icon from "@/assets/components/ui/Icon"
+import Button from "@/assets/components/ui/Button"
+
 export default function Header() {
   return (
     <header className={style.header}>
-      <>header</>
+      <Icon icon="logo" />
+      <nav className={style.nav}>
+        <Button type="text" link="/marketplace">
+          Marketplace
+        </Button>
+        <Button type="text" link="/rankings">
+          Rankings
+        </Button>
+        <Button type="text" link="/walletconnect">
+          Connect a wallet
+        </Button>
+        <Button type="primary">
+          <Icon icon="user" />
+          <p className={style.button}>Sign Up</p>
+        </Button>
+      </nav>
     </header>
   )
 }
